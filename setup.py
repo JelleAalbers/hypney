8b0245e9f70aeb2713ee0b1044b0370322ae2ab1
Jelle Aalbers <jelleaalbers@gmail.com>
2021-04-08 16:02:04 -0700

    Package scaffold

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requires= [x.strip() for x in f.readlines()]
+
+with open('README.md') as file:
+    readme = file.read()
+
+with open('HISTORY.md') as file:
+    history = file.read()
+
+setuptools.setup(
+    name='hypney',
+    version='0.0.0',
+    description='Hypothesis testing with Neyman constructions',
+    url='https://github.com/JelleAalbers/hypney',
+    python_requires=">=3.6",
+    setup_requires=['pytest-runner'],
+    install_requires=requires,
+    tests_require=['pytest'],
+    long_description=readme + '\n\n' + history,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    zip_safe=False)
