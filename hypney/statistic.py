0dd7e0066be372cb314624dcefe01aba1b5a60b1
Jelle Aalbers <jelleaalbers@gmail.com>
2021-09-07 16:09:55 -0700

    More Statistic basics

--- hypney/statistic.py
+++ hypney/statistic.py
@@ -1,63 +1,144 @@
-import inspect
+from copy import copy
 
 import numpy as np
+from scipy import stats
 
 import hypney
 
 export, __all__ = hypney.exporter()
 
 
-class Statistic(hypney.Element):
+class Statistic:
+    data_dependent = True
+    param_dependent = True
+    keep_data = True
+    data = None
+
+    def __init__(self, model: hypney.Model, data=None):
+        self.model = model
+        self._set_data(data)
+
+        if not self.param_dependent:
+            self.keep_data = False
+
+    def _set_data(self, data=None):
+        if data is None:
+            return
+
+        data = self.model.validate_data(data)
+        self.data = data
+        self.init_data()
+
+        if not self.param_dependent:
+            # Precompute result on this data. See __call__ for special case
+            # where this is returned
+            self._result = self.compute()
+        if not self.keep_data:
+            # Statistic relies only on stuff computed in init_data
+            # (and possibly params); replace self.data with something other
+            # than None that takes no memory
+            self.data = True
+
+    def init_data(self):
+        # Child classes may wish to do some computations here
+        pass
+
+    def __call__(self, *args, data=None, params=None):
+
+        if len(args) == 0:
+            # data and params passed by keyword
+            pass
+
+        elif len(args) == 2:
+            # data and params passed as positional arguments
+            # No keywords can be passed:
+            assert (
+                params is None and data is None
+            ), "Statistic takes at most 2 arguments"
+            data, params = args
+
+        elif len(args) == 1:
+            # One positional argument... harder case.
+            if params is None and data is None:
+                # No keywords provided.
+                # Data must have been set on init, and args[0] is params
+                assert self.data is not None, "Must provide data"
+                params = args[0]
+            elif params is None:
+                # Data provided by keyword; positional arg must be params
+                params = args[0]
+            elif data is None:
+                # Params provided by keyword; positional arg must be data
+                data = args[0]
+            else:
+                raise ValueError("Statistic takes at most 2 arguments")
+        else:
+            raise ValueError("Statistic takes at most 2 arguments")
+
+        if data is not None:
+            # Work on a copy of self with the new data set
+            self = copy(self)
+            self._set_data(data)
+
+        elif not self.param_dependent:
+            # Data has not changed, and doesn't depend on parameters:
+            # use precomputed result
+            return self._result
+
+        params = self.model.validate_params(params)
+        return self.compute(params)
+
+    def compute(self, params):
+        raise NotImplementedError
+
+
+class LogLikelihood(Statistic):
+    def compute(self, params):
+        return -self.model.rate(params) + np.sum(
+            np.log(self.model.diff_rate(self.data, params))
+        )
+
+
+class LogLikelihoodRatio(Statistic):
+    def __init__(self, *args, max_estimator=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if max_estimator is None:
+            max_estimator = hypney.Maximum
+        self.ll = LogLikelihood(self.model)
+        self.mle = max_estimator(self.ll)
+
+    def init_data(self):
+        self.bestfit = self.mle(self.data)
+        self.ll_bestfit = self.ll(self.mle, self.data)
+
+    def compute(self, params):
+        return self.ll(params, self.data) - self.ll_bestfit
+
+
+class Count(Statistic):
+    param_dependent = False
+
+    def compute(self):
+        return self.n
+
+    def pdf(self, params):
+        return stats.poisson(mu=self.model.rate(params)).pmf
+
+
+class Mean(Statistic):
+    # TODO: specify which dimension to average over
+    param_dependent = False
+
+    def compute(self):
+        return np.mean(self.data)
+
+
+class Prior(Statistic):
+    data_dependent = False
 
     def __call__(self, data, params):
         raise NotImplementedError
 
-    @classmethod
-    def from_function(cls, f, model: hypney.Model = None, param_specs=None):
-        self = cls()
 
-        f_params = list(inspect.signature(f).parameters.values())
-        assert f_params[0].name == "data", "First argument must be data"
-
-        if f_params[1].name == "params":
-            if model is not None:
-                # Take param specs from model
-                specs = model.param_specs
-            else:
-                # Take explicitly provided specs
-                specs = param_specs
-                assert isinstance(specs, tuple)
-                assert all([isinstance(x, hypney.ParameterSpec) for x in specs])
-
-            self.param_specs = specs
-            self.__call__ = f
-
-        else:
-            # Construct parameter spec from the function signature
-            # params_specs argument is now an optional as dict with options
-            # TODO: Confusing! Make separate arg/kwarg!!
-            if param_specs is None:
-                spec = tuple()
-            spec = tuple(
-                [
-                    hypney.ParameterSpec(
-                        name=p.name,
-                        default=p.default,
-                        **param_specs.get(p.name, dict())
-                    )
-                    for p in f_params[1:]
-                ]
-            )
-            assert not any([p.default == inspect.Parameter.empty for p in spec])
-
-            self.__call__ = lambda data, params: f(data, **params)
-
-
-class LogLikelihood(Statistic):
-    def __init__(self, model: hypney.Model):
-        self.model = model
-
-    def __call__(self, data, params):
-        return -self.model.rate(params) + np.sum(
-            np.log(self.model.diff_rate(data, params))
-        )
+Constraint = Prior
