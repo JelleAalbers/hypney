logpdf fixes, numerical stability for log likelihood

hypney/statistic.py
@@ -23,7 +23,6 @@ class Statistic:
     def _set_dist(self, dist: hypney.Model):
         if dist is hypney.NotChanged:
             return
-        print("setting dist")
         if dist is None:
             if hasattr(self, "_build_dist"):
                 # Build a distribution automatically
@@ -113,7 +112,7 @@ class Statistic:
         # Default assumption is that distribution is parameter-free
         return dict()
 
-    def dist_from_toys(self, params=None, n_toys=1000, **kwargs):
+    def dist_from_toys(self, params=None, n_toys=1000, transform=np.asarray, **kwargs):
         """Return an estimated distribution of the statistic given params
         from running simulations.
 
@@ -122,16 +121,18 @@ class Statistic:
         # Use a *lot* of bins by default, since we're most interested
         # in the cdf/ppf
         kwargs.setdefault("bin_count_multiplier", 10)
-        toys = self.rvs(n_toys, params=params)
+        toys = transform(self.rvs(n_toys, params=params))
         dist = hypney.models.from_samples(toys, **kwargs)
         # Remove standard loc/scale/rate params
         # to avoid confusion with model parameters
         return dist.freeze()
 
-    def interpolate_dist_from_toys(self, anchors: dict, **kwargs):
+    def interpolate_dist_from_toys(self, anchors: dict, progress=True, **kwargs):
         assert isinstance(anchors, dict), "Pass a dict of sequences as anchors"
         return hypney.models.Interpolation(
-            functools.partial(self.dist_from_toys, **kwargs), anchors
+            functools.partial(self.dist_from_toys, **kwargs),
+            anchors,
+            progress=progress,
         )
 
 

hypney/utils/eagerpy.py
@@ -1,10 +1,12 @@
 import math
+import functools
 import typing as ty
 
 import hypney
 
 import eagerpy as ep
 import numpy as np
+from scipy import special
 
 export, __all__ = hypney.exporter()
 
@@ -32,13 +34,6 @@ def sequence_to_tensor(x: ty.Sequence, *, match_type: ep.TensorType):
     if isinstance(match_type, ep.TensorFlowTensor):
         return ep.tensorflow.convert_to_tensor(x, dtype=match_type.dtype)
     raise ValueError(f"match_type of unknown type {type(match_type)}")
-
-
-def log(x):
-    if isinstance(x, ep.Tensor):
-        return ep.log(x)
-    # Probably a scalar...
-    return math.log(x)
 
 
 @export
@@ -90,8 +85,7 @@ def split(x, *args, **kwargs):
 
 @export
 def tensorlib(x: ep.TensorType):
-    # This looks like
-    if isinstance(x, ep.NumPyTensor):
+    if isinstance(x, ep.NumPyTensor) or x is None:
         return ep.numpy
     elif isinstance(x, ep.PyTorchTensor):
         return ep.torch
@@ -114,3 +108,13 @@ def bucketize(x, p):
     elif isinstance(x, ep.JAXTensor):
         return ep.jax.numpy.searchsorted(p, x)
     raise TypeError(f"Unknown tensor type {x}")
+
+
+@export
+def logsumexp(tensor, axis=0):
+    if isinstance(tensor, ep.NumPyTensor):
+        result = special.logsumexp(tensor.raw, axis=axis)
+        return ep.astensor(np.asarray(result))
+    # TODO: tf/torch/jax only support two-argument function logaddexp.
+    # Do something horrible for now:
+    return ep.log(ep.sum(ep.exp(tensor), axis=axis))

tests/models/test_scipy_stats.py
@@ -39,7 +39,9 @@ def test_uniform():
     np.testing.assert_array_equal(m.ppf([0.0, 0.5, 1.0]), np.array([0.0, 0.5, 1.0]))
 
     # Test diff rate
-    assert m.diff_rate(0.0) == 0.0
+    m = hypney.models.uniform(rate=2)
+    assert m.diff_rate(0.0) == 2.0
+    assert m.log_diff_rate(0.0) == np.log(2.0)
 
     # Test mean and std
     assert m.mean() == 0.5

tests/stats/test_likelihood.py
@@ -5,15 +5,16 @@ import hypney
 
 
 def test_likelihood():
-    # Single event at x=0
+    # Five events at x=0
     m = hypney.models.norm(rate=1)
-    data = np.array([0,])
+    data = np.array([0, 0, 0, 0, 0])
 
     lf = hypney.statistics.LogLikelihood(m, data=data)
-    assert len(lf.data) == 1
+    assert len(lf.data) == 5
 
-    assert lf() == -1 + stats.norm.logpdf(0)
-    assert lf(params=dict(rate=2)) == -2 + np.log(2 * stats.norm.pdf(0))
+    assert lf() == -1 + 5 * stats.norm.logpdf(0)
+
+    assert lf(params=dict(rate=2)) == -2 + 5 * np.log(2 * stats.norm.pdf(0))
 
 
 def test_lr():