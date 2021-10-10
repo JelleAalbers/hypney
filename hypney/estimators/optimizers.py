a076742646c7c603ae1d2f021e529c74735a5f12
Jelle Aalbers <jelleaalbers@gmail.com>
2021-10-09 19:55:33 -0700

    Refactor optimizer

--- hypney/estimators/optimizers.py
+++ hypney/estimators/optimizers.py
@@ -1,3 +1,4 @@
+from copy import copy
 import warnings
 
 import eagerpy as ep
@@ -60,13 +61,15 @@ class Minimum(hypney.Estimator):
             on_failure = self.on_failure if final_method else "raise"
             try:
                 return self._minimize(method, on_failure=on_failure)
-            except Exception:
+            except Exception as e:
                 if final_method:
-                    raise
+                    raise e
+                elif self.on_failure in ('warn', 'raise'):
+                    warnings.warn(f"Optimization with {method} failed: {e}")
                 continue
 
     def _minimize(self, method, on_failure):
-        history = []
+        self.history = []
         guess = np.array(
             [
                 hypney.utils.eagerpy.ensure_numpy_float(p.default)
@@ -81,87 +84,12 @@ class Minimum(hypney.Estimator):
         else:
             autograd = self.autograd
 
-        if not autograd:
-            jac = None
-
-            def fun(params):
-                nonlocal history
-                if not self.in_bounds(params):
-                    result = float("nan")
-                else:
-                    try:
-                        result = self.sign * self.stat.compute(
-                            params=self._param_sequence_to_dict(params)
-                        )
-                    except ValueError as e:
-                        # pytorch actually crashes when you pass scale=0
-                        result = float("nan")
-                    # Giving float(inf) to optimizer can throw off convergence tests
-                    # based on estimated function value. NaN seems safer.
-                    if not np.isfinite(result):
-                        result = float("nan")
-                history.append([params, result, None])
-                return result
-
-        else:
-            jac = True
-
-            def _fun(params):
-                result, grad = ep.value_and_grad(
-                    lambda param_tensor: self.sign
-                    * self.stat.model._to_tensor(
-                        self.stat.compute(
-                            # [:,None] ensures batch_shape = 1, so Model._scalar_method
-                            # won't un-tensorify the result...
-                            params=self._param_sequence_to_dict(param_tensor[:, None])
-                        )
-                    ),
-                    params,
-                )
-                return result, grad
-
-            def fun(params):
-                nonlocal history
-                if not self.in_bounds(params):
-                    result = float("nan")
-                    grad = np.ones(len(params)) * float("nan")
-                else:
-                    param_tensor = self.stat.model._to_tensor(params)
-                    # [:,None] ensures batch_shape = 1, so Model._scalar_method
-                    # won't do the un-tensorifying .item() on the result... kludge?
-                    param_tensor = param_tensor[:, None]
-                    try:
-                        result, grad = ep.value_and_grad(
-                            lambda _ps: self.sign
-                            * self.stat.model._to_tensor(
-                                self.stat.compute(
-                                    params=self._param_sequence_to_dict(_ps)
-                                )
-                            ),
-                            param_tensor,
-                        )
-                    except ValueError:
-                        result = float("nan")
-                        grad = np.ones(len(params)) * float("nan")
-                    else:
-                        # JAX, and possibly other libraries, change the precision
-                        # to 32-bit without asking. Scipy.optimize doesn't expect this
-                        # and would throw a strange error if we don't cast back to 64:
-                        # "failed to initialize intent(inout) array -- input not
-                        #  fortran contiguous -- expected elsize=8 but got 4"
-                        result = ep_util.np64(result).item()
-                        grad = ep_util.np64(grad)
-                    if not np.isfinite(result):
-                        result *= float("nan")
-                        grad *= float("nan")
-                # grad.copy() since optimizer might modify it in-place
-                # (e.g. LBFGS-B seems to replace NaN with the previous value)
-                history.append([params, result, grad.copy()])
-                return result, grad
-
         result = optimize.minimize(
-            fun=fun,
-            jac=jac,
+            fun=self._objective
+            if autograd
+            else lambda x, autograd: self._objective(x, autograd)[0],
+            jac=True if autograd else None,
+            args=(autograd,),
             x0=guess,
             bounds=bounds,
             options=self.options,
@@ -191,9 +119,57 @@ class Minimum(hypney.Estimator):
         elif self.return_result == "optresult":
             return result
         elif self.return_result == "optresult_and_history":
-            return result, history
+            return result, self.history
         else:
             raise ValueError(f"Unknown return_result {self.return_result}")
+
+    def _objective(self, params, autograd):
+        nan_result = float("nan"), np.ones(len(params)) * float("nan")
+
+        if not self.in_bounds(params):
+            return nan_result
+        try:
+            result, grad = self._inner_objective(params, autograd)
+        except ValueError as e:
+            # e.g. pytorch actually crashes when you pass scale=0
+            return nan_result
+        # JAX, and possibly other libraries, change the precision
+        # to 32-bit without asking. Scipy.optimize doesn't expect this
+        # and would throw a strange error if we don't cast back to 64:
+        # "failed to initialize intent(inout) array -- input not
+        #  fortran contiguous -- expected elsize=8 but got 4"
+        if isinstance(result, ep.Tensor):
+            result = ep_util.np64(result).item()
+        if isinstance(grad, ep.Tensor):
+            grad = ep_util.np64(grad)
+        elif grad is None:
+            grad = nan_result[1]
+        # Giving float(inf) to optimizer can throw off convergence tests
+        # based on the estimated function value. NaN seems safer.
+        if not np.isfinite(result):
+            return nan_result
+
+        # copy(grad) is needed since e.g. L-BFGS-B modifies it in-place
+        self.history.append([params, result, copy(grad)])
+        return result, grad
+
+    def _inner_objective(self, params, autograd):
+        if autograd:
+            # [:,None] ensures batch_shape = 1, so Model._scalar_method
+            # won't do the un-tensorifying .item() on the result... kludge?
+            param_tensor = self.stat.model._to_tensor(params)[:, None]
+            return ep.value_and_grad(
+                lambda _ps: self.sign
+                * self.stat.model._to_tensor(
+                    self.stat.compute(params=self._param_sequence_to_dict(_ps))
+                ),
+                param_tensor,
+            )
+        else:
+            result = self.sign * self.stat.compute(
+                params=self._param_sequence_to_dict(params)
+            )
+            return result, None
 
 
 @export
