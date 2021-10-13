c6a3d85f99e4c955f78ebdbc84e63ea4234c9929
Jelle Aalbers <jelleaalbers@gmail.com>
2021-10-12 18:54:20 -0700

    More sensible optimizer return  spec

--- hypney/estimators/optimizers.py
+++ hypney/estimators/optimizers.py
@@ -14,14 +14,14 @@ export, __all__ = hypney.exporter()
 @export
 class Minimum(hypney.Estimator):
     sign = 1
-    return_result = "point"
+    return_kind = "point"
 
     def __init__(
         self,
         *args,
         options=None,
         method=("L-BFGS-B", "powell"),
-        return_result=None,
+        return_kind=None,
         on_failure="raise",
         autograd=None,
         **kwargs,
@@ -32,8 +32,10 @@ class Minimum(hypney.Estimator):
             options = dict()
         self.options = options
 
-        if return_result:
-            self.return_result = return_result
+        if return_kind:
+            self.return_kind = return_kind
+        if isinstance(self.return_kind, str):
+            self.return_kind = (self.return_kind,)
 
         if isinstance(method, str):
             method = (method,)
@@ -45,15 +47,6 @@ class Minimum(hypney.Estimator):
             autograd = self.stat.model._backend_name != "numpy"
         self.autograd = autograd
 
-    def in_bounds(self, params):
-        if not isinstance(params, dict):
-            params = self._param_sequence_to_dict(params)
-        for p in self._free_params():
-            v = params[p.name]
-            if not p.min <= v <= p.max:
-                return False
-        return True
-
     def _compute(self):
         # Try different methods; complaining only if last one fails.
         for i, method in enumerate(self.methods):
@@ -64,7 +57,7 @@ class Minimum(hypney.Estimator):
             except Exception as e:
                 if final_method:
                     raise e
-                elif self.on_failure in ('warn', 'raise'):
+                elif self.on_failure in ("warn", "raise"):
                     warnings.warn(f"Optimization with {method} failed: {e}")
                 continue
 
@@ -84,10 +77,13 @@ class Minimum(hypney.Estimator):
         else:
             autograd = self.autograd
 
-        result = optimize.minimize(
-            fun=self._objective
+        fun = (
+            self.objective
             if autograd
-            else lambda x, autograd: self._objective(x, autograd)[0],
+            else lambda x, autograd: self.objective(x, autograd)[0]
+        )
+        result = optimize.minimize(
+            fun=fun,
             jac=True if autograd else None,
             args=(autograd,),
             x0=guess,
@@ -107,32 +103,38 @@ class Minimum(hypney.Estimator):
             else:
                 raise ValueError(f"Unknown on_failure {self.on_failure}")
 
-        bestfit = self._param_sequence_to_dict(result.x)
-        value = result.fun * self.sign
+        possible_returns = dict(
+            point=self._param_sequence_to_dict(result.x),
+            value=result.fun * self.sign,
+            optresult=result,
+            history=self.history,
+        )
+        return [possible_returns[x] for x in self.return_kind]
 
-        if self.return_result == "point":
-            return bestfit
-        elif self.return_result == "point_and_value":
-            return bestfit, value
-        elif self.return_result == "value":
-            return value
-        elif self.return_result == "optresult":
-            return result
-        elif self.return_result == "optresult_and_history":
-            return result, self.history
-        else:
-            raise ValueError(f"Unknown return_result {self.return_result}")
+    def objective(self, params, autograd):
+        result, grad = self._objective(params, autograd)
+        # copy(grad) is needed since e.g. L-BFGS-B modifies it in-place
+        self.history.append([params, result, copy(grad)])
+        return result, grad
 
     def _objective(self, params, autograd):
-        nan_result = float("nan"), np.ones(len(params)) * float("nan")
+        if autograd:
+            nan_result = float("nan"), np.ones(len(params)) * float("nan")
+        else:
+            nan_result = float("nan"), None
 
-        if not self.in_bounds(params):
-            return nan_result
+        # Bounds check
+        for i, p in enumerate(self._free_params()):
+            if not p.min <= params[i] <= p.max:
+                return nan_result
+
+        # Evaluate the statistic, possibly under autograd
         try:
-            result, grad = self._inner_objective(params, autograd)
+            result, grad = self._innermost_objective(params, autograd)
         except ValueError as e:
             # e.g. pytorch actually crashes when you pass scale=0
             return nan_result
+
         # JAX, and possibly other libraries, change the precision
         # to 32-bit without asking. Scipy.optimize doesn't expect this
         # and would throw a strange error if we don't cast back to 64:
@@ -141,19 +143,14 @@ class Minimum(hypney.Estimator):
         if isinstance(result, ep.Tensor):
             result = ep_util.np64(result).item()
         if isinstance(grad, ep.Tensor):
-            grad = ep_util.np64(grad)
-        elif grad is None:
-            grad = nan_result[1]
+            grad = ep_util.np64(grad).ravel()
         # Giving float(inf) to optimizer can throw off convergence tests
         # based on the estimated function value. NaN seems safer.
         if not np.isfinite(result):
             return nan_result
-
-        # copy(grad) is needed since e.g. L-BFGS-B modifies it in-place
-        self.history.append([params, result, copy(grad)])
         return result, grad
 
-    def _inner_objective(self, params, autograd):
+    def _innermost_objective(self, params, autograd):
         if autograd:
             # [:,None] ensures batch_shape = 1, so Model._scalar_method
             # won't do the un-tensorifying .item() on the result... kludge?
@@ -174,13 +171,12 @@ class Minimum(hypney.Estimator):
 
 @export
 class MinimumAndValue(Minimum):
-    return_result = "point_and_value"
+    return_kind = ("point", "value")
 
 
 @export
 class MaximumAndValue(MinimumAndValue):
     sign = -1
-    return_result = "point_and_value"
 
 
 @export
