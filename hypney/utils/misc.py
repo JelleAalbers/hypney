20de20a1299c7379c61dd8d5c6fad708112888b9
Jelle Aalbers <jelleaalbers@gmail.com>
2022-03-07 18:30:10 -0800

    Move poisson limit helpers to utils

--- hypney/utils/misc.py
+++ hypney/utils/misc.py
@@ -1,6 +1,9 @@
 from functools import partial
 import warnings
 
+import numpy as np
+from scipy import stats
+
 import hypney
 
 export, __all__ = hypney.exporter()
@@ -29,3 +32,26 @@ def flexible_clip(x, xmin=-float("inf"), xmax=float("inf")):
     if isinstance(x, (float, int)):
         return min(max(x, xmin), xmax)
     return x.clip(xmin, xmax)
+
+
+@export
+def poisson_ul(n, mu_bg=0, cl=0.9):
+    """Upper limit on mu_signal, from observing n events
+    where mu_bg background events were expected
+
+    NB: can be negative if mu_bg large enough.
+    It's your responsibility to clip to 0...
+    """
+    # Adapted from https://stackoverflow.com/a/14832525
+    return stats.chi2.ppf(cl, 2 * n + 2) / 2 - mu_bg
+
+
+@export
+def poisson_ll(n, cl=0.9):
+    # Adapted from https://stackoverflow.com/a/14832525
+    # Checked through
+    #     n = np.arange(0, 100)
+    #     stats.poisson(poisson_ul(n)).cdf(n)
+    # (and similarly for upper limit)
+    n = np.asarray(n)
+    return np.where(n == 0, np.zeros_like(n), stats.chi2.ppf(1 - cl, 2 * n + 2) / 2)
--- tests/estimators/test_confidence_interval.py
+++ tests/estimators/test_confidence_interval.py
@@ -5,36 +5,15 @@ from scipy import stats
 import hypney.all as hp
 
 
-def poisson_ul(n, mu_bg=0, cl=0.9):
-    """Upper limit on mu_signal, from observing n events
-    where mu_bg background events were expected
-
-    NB: can be negative if mu_bg large enough.
-    It's your responsibility to clip to 0...
-    """
-    # Adapted from https://stackoverflow.com/a/14832525
-    return stats.chi2.ppf(cl, 2 * n + 2) / 2 - mu_bg
-
-
-def poisson_ll(n, cl=0.9):
-    # Adapted from https://stackoverflow.com/a/14832525
-    # Checked through
-    #     n = np.arange(0, 100)
-    #     stats.poisson(poisson_ul(n)).cdf(n)
-    # (and similarly for upper limit)
-    n = np.asarray(n)
-    return np.where(n == 0, np.zeros_like(n), stats.chi2.ppf(1 - cl, 2 * n + 2) / 2)
-
-
 def test_poisson_upper_limit():
     m = hp.uniform(data=np.array([]))
     stat = hp.Count(m)
 
     ul = hp.UpperLimit(stat, poi="rate", anchors=[0, 5], cl=0.9, use_cdf=False)()
-    np.testing.assert_allclose(ul, poisson_ul(0))
+    np.testing.assert_allclose(ul, hp.poisson_ul(0))
 
     ul = hp.UpperLimit(stat, poi="rate", anchors=[0, 5], cl=0.9, use_cdf=True)()
-    np.testing.assert_allclose(ul, poisson_ul(0))
+    np.testing.assert_allclose(ul, hp.poisson_ul(0))
 
     # Invalid anchor
     with pytest.raises(ValueError):
@@ -64,13 +43,13 @@ def test_poisson_upper_limit():
     )
     stat3 = hp.Count(mneg)
     ul = hp.UpperLimit(stat3, poi="neg_rate", anchors=[-20, 0], cl=0.9, sign=-1)()
-    assert ul == -poisson_ll(0) == 0
+    assert ul == -hp.poisson_ll(0) == 0
 
     # Now data is not empty, UL finite (and negative)
     ul = hp.UpperLimit(
         stat3(data=np.ones(50)), poi="neg_rate", anchors=[-50, -20], cl=0.9, sign=-1
     )()
-    np.testing.assert_allclose(ul, -poisson_ll(50))
+    np.testing.assert_allclose(ul, -hp.poisson_ll(50))
 
 
 def test_poisson_lower_limit():
@@ -78,7 +57,7 @@ def test_poisson_lower_limit():
     stat = hp.Count(m)
 
     ll = hp.LowerLimit(stat, poi="rate", anchors=[0, 5], cl=0.9, use_cdf=False)()
-    np.testing.assert_allclose(ll, poisson_ll(0))
+    np.testing.assert_allclose(ll, hp.poisson_ll(0))
 
     # Test case where statistic decreases as parameter increases
     mneg = m.reparametrize(
@@ -89,12 +68,12 @@ def test_poisson_lower_limit():
     )
     stat3 = hp.Count(mneg)
     ll = hp.LowerLimit(stat3, poi="neg_rate", anchors=[-20, 0], cl=0.9, sign=-1)()
-    np.testing.assert_allclose(ll, -poisson_ul(0))
+    np.testing.assert_allclose(ll, -hp.poisson_ul(0))
 
     ll = hp.LowerLimit(
         stat3(data=np.ones(50)), poi="neg_rate", anchors=[-100, -50], cl=0.9, sign=-1
     )()
-    np.testing.assert_allclose(ll, -poisson_ul(50))
+    np.testing.assert_allclose(ll, -hp.poisson_ul(50))
 
 
 def test_poisson_central_interval():
@@ -102,10 +81,10 @@ def test_poisson_central_interval():
     stat = hp.Count(m)
     ll, ul = hp.CentralInterval(stat, poi="rate", anchors=[0, 5], cl=0.8)()
     assert ll == 0
-    np.testing.assert_allclose(ul, poisson_ul(0))
+    np.testing.assert_allclose(ul, hp.poisson_ul(0))
 
     ll, ul = hp.CentralInterval(
         stat(data=np.zeros(50)), poi="rate", anchors=[10, 50, 90], cl=0.8
     )()
-    np.testing.assert_allclose(ll, poisson_ll(50))
-    np.testing.assert_allclose(ul, poisson_ul(50))
+    np.testing.assert_allclose(ll, hp.poisson_ll(50))
+    np.testing.assert_allclose(ul, hp.poisson_ul(50))
